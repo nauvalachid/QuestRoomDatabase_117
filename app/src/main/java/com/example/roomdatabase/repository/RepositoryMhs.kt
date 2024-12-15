10a8fcd78c863fad18c2cf8927903cd67b6024aa
nauvalachid <nauvalachid@gmail.com>
2024-12-16 00:45:51 +0700

    Menambahkan deklarasi fungsi CRUD untuk pengelolaan data Mahasiswa di RepositoryMhs

--- app/src/main/java/com/example/roomdatabase/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomdatabase/repository/RepositoryMhs.kt
@@ -1,7 +1,16 @@
 package com.example.roomdatabase.repository
 
 import com.example.roomdatabase.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs() : Flow<List<Mahasiswa>>
+
+    fun getMhs(nim : String) : Flow<Mahasiswa>
+
+    suspend fun deleteMhs(Mahasiswa: Mahasiswa)
+
+    suspend fun updateMhs(Mahasiswa: Mahasiswa)
 }
--- app/src/main/java/com/example/roomdatabase/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/ui/viewmodel/HomeMhsViewModel.kt
@@ -2,7 +2,33 @@ package com.example.roomdatabase.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.roomdatabase.repository.RepositoryMhs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.internal.NopCollector.emit
 
 class HomeMhsViewModel(
     private val repositoryMhs: RepositoryMhs
-) : ViewModel()
+) : ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+}
