0fd6b0d15945eccce51febf05731eca777c30944
nauvalachid <nauvalachid@gmail.com>
2024-12-16 01:01:54 +0700

    Menambahkan ViewModel untuk menampilkan detail mahasiswa menggunakan SavedStateHandle dan repository

--- app/src/main/java/com/example/roomdatabase/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.roomdatabase.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.roomdatabase.repository.RepositoryMhs
+
+class DetailMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+) : ViewModel()
