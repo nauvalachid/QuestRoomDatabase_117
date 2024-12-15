ff4a69f929b68018b46b599d2333f7a0eb09f3cf
nauvalachid <nauvalachid@gmail.com>
2024-12-16 00:49:56 +0700

    Menggunakan stateIn untuk mengelola StateFlow dengan initial value dan menambahkan data class HomeUiState untuk UI state

--- app/src/main/java/com/example/roomdatabase/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,19 +1,24 @@
 package com.example.roomdatabase.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomdatabase.data.entity.Mahasiswa
 import com.example.roomdatabase.repository.RepositoryMhs
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeMhsViewModel(
     private val repositoryMhs: RepositoryMhs
 ) : ViewModel() {
 
-    val homeUiState: Flow<Unit> = repositoryMhs.getAllMhs()
+    val homeUiState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
         .filterNotNull()
         .map {
             HomeUiState(
@@ -34,4 +39,19 @@ class HomeMhsViewModel(
                 )
             )
         }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+
+    data class HomeUiState(
+        val listMhs: List<Mahasiswa> = listOf(),
+        val isLoading: Boolean = false,
+        val isError: Boolean = false,
+        val errorMessage: String = ""
+    )
+
 }
