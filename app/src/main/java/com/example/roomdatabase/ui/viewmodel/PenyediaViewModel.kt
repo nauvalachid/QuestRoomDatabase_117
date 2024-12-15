2df16c405809704b4b35c1424bd200e6a8f81d8f
nauvalachid <nauvalachid@gmail.com>
2024-12-16 01:40:51 +0700

    Membuat file HomeMhsVIew.kt di package view.mahasiswa

--- app/src/main/java/com/example/roomdatabase/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/ui/viewmodel/PenyediaViewModel.kt
@@ -1,20 +1,53 @@
 package com.example.roomdatabase.ui.viewmodel
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomdatabase.KrsApp
+import kotlinx.coroutines.launch
 
-object PenyediaViewModel {
+object PenyediaViewModel{
 
-    val factory = viewModelFactory {
+    val Factory = viewModelFactory {
         initializer {
             MahasiswaViewModel(
                 krsApp().containerApp.repositoryMhs
             )
         }
+
         initializer {
             HomeMhsViewModel(
                 krsApp().containerApp.repositoryMhs
