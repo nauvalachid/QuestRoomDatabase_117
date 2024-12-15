69013ed7efa8f028c06ba6b122e38d010d8141a0
nauvalachid <nauvalachid@gmail.com>
2024-12-16 00:39:23 +0700

    Menambahkan HomeMhsViewModel untuk mengelola data mahasiswa dari RepositoryMhs

--- app/src/main/java/com/example/roomdatabase/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,2 +1,8 @@
 package com.example.roomdatabase.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.example.roomdatabase.repository.RepositoryMhs
+
+class HomeMhsViewModel(
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel()
